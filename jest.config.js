c591e221056a4287a1b247bbff24d050a7c4c937
AlineFreitas <aline.freitas@thoughworks.com>
2021-05-17 23:36:48 -0300

    Instala e configura jest para typescript

---
+++ jest.config.js
@@ -0,0 +1,88 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/en/configuration.html
+ */
+
+module.exports = {
+  // Automatically clear mock calls and instances between every test
+  // clearMocks: false,
+
+  // Indicates whether the coverage information should be collected while executing the test
+  // collectCoverage: false,
+
+  // An array of glob patterns indicating a set of files for which coverage information should be collected
+  collectCoverageFrom: [
+    "<rootDir>/**/**.ts",
+    "!<rootDir>/**/**.{test,spec}.ts"
+  ],
+
+  // The directory where Jest should output its coverage files
+  coverageDirectory: "../coverage",
+
+  // Indicates which provider should be used to instrument code for coverage
+  coverageProvider: "v8",
+
+  // A list of reporter names that Jest uses when writing coverage reports
+  // coverageReporters: [
+  //   "json",
+  //   "text",
+  //   "lcov",
+  //   "clover"
+  // ],
+
+  // An object that configures minimum threshold enforcement for coverage results
+  coverageThreshold: {
+    "global": {
+      "branches": 50,
+      "functions": 50,
+      "lines": 50
+    }
+  },
+
+  // An array of file extensions your modules use
+  moduleFileExtensions: [
+    "js",
+    "ts",
+  ],
+
+  // A preset that is used as a base for Jest's configuration
+  // preset: undefined,
+
+  // The root directory that Jest should scan for tests and modules within
+  // rootDir: 'src',
+
+  // A list of paths to directories that Jest should use to search for files in
+  roots: [
+    "src",
+    "test"
+  ],
+
+  // Allows you to use a custom runner instead of Jest's default test runner
+  // runner: "jest-runner",
+
+  // The paths to modules that run some code to configure or set up the testing environment before each test
+  // setupFiles: [],
+
+  // The number of seconds after which a test is considered as slow and reported as such in the results.
+  slowTestThreshold: 5,
+
+  // The test environment that will be used for testing
+  testEnvironment: "node",
+
+  // The glob patterns Jest uses to detect test files
+  testMatch: [
+    "**/__tests__/**/*.[jt]s?(x)",
+    "**/?(*.)+(spec|test).[tj]s?(x)"
+  ],
+
+  // A map from regular expressions to paths to transformers
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+
+  // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
+  // transformIgnorePatterns: [
+  //   "/node_modules/",
+  //   "\\.pnp\\.[^\\/]+$"
+  // ],
+};

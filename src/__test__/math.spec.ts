beb502d870852ee95aa51d3df6011d2807ba12be
AlineFreitas <aline.freitas@thoughworks.com>
2021-05-20 01:48:28 -0300

    Remove arquivos desnecessários

--- src/__test__/math.spec.ts
+++
@@ -1,7 +0,0 @@
-import { sum, multiply } from '../math'
-describe('Index', () => {
-  it('Does math right', () => {
-    expect(sum(2, 3)).toEqual(5)
-    expect(multiply(5, 5)).toEqual(25)
-  })
-})
--- src/math.ts
+++
@@ -1,9 +0,0 @@
-function sum(number1: number, number2: number) {
-  return number1 + number2;
-}
-
-function multiply(number1: number, number2: number) {
-  return number1 * number2;
-}
-
-export { sum, multiply };
